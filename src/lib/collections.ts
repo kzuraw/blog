2d72d93c6b150d5b31bab48f589bca3a47332ff7
Krzysztof Żuraw <9116238+kzuraw@users.noreply.github.com>
2026-04-01 15:29:18 +0200

    Fix builds after astro 6 update

--- src/lib/collections.ts
+++ src/lib/collections.ts
@@ -3,6 +3,6 @@ import { getCollection } from "astro:content";
 export const getBlogPosts = async (limit?: number) => {
   const posts = (await getCollection("blog"))
     .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
-    .map((post) => ({ ...post, urlPath: `/blog/${post.slug}` }));
+    .map((post) => ({ ...post, urlPath: `/blog/${post.id}` }));
   return limit ? posts.slice(0, limit) : posts;
 };
